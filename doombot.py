55e4d7e885274f07fde32719127ffb21c05c7358
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-06-19 13:04:30 +0700

    Update doombot.py

--- doombot.py
+++ doombot.py
@@ -1,14 +1,15 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
-import asyncio
-import platform
-import colorsys
-import random
-import os
-import time
-from discord.voice_client import VoiceClient
-from discord import Game, Embed, Color, Status, ChannelType
+import datetime
 
-client.run(os.getenv('Token'))
+from urllib import parse, request
+import re
+
+bot = commands.Bot(command_prefix='wox', description="This is DooM Bot")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+bot.run(os.getenv('Token'))
