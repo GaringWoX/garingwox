1148f274b98e10ca0c459e6317ffc3a09a952405
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-06-19 12:51:57 +0700

    Update doombot.py

--- doombot.py
+++ doombot.py
@@ -11,31 +11,10 @@ import time
 from discord.voice_client import VoiceClient
 from discord import Game, Embed, Color, Status, ChannelType
 
+client = commands.Bot(command_prefix='wox')
 
-Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
-client = Bot(description="DooMBoT Bot is best", command_prefix="d!", pm_help = True)
-client.remove_command('help')
-
-
-async def status_task():
-    while True:
-        await client.change_presence(game=discord.Game(name='for d!help'))
-        await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
-        await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
-        await asyncio.sleep(5)
-        
-
-	
-@client.event
-async def on_ready():
-    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
-    print('--------')
-    print('--------')
-    print('Started DooM BOT')
-    print('Created by DooM')
-    client.loop.create_task(status_task())
-
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 client.run(os.getenv('Token'))
