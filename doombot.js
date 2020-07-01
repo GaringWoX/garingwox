827a73a55895bd78f57a10c80d158fdfb9d2c46f
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-07-02 06:38:42 +0700

    Update doombot.js

--- doombot.js
+++ doombot.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
     client.user.setActivity("Take The Pill & Chill", { type: "WATCHING"})
-})
+});
 
 
 client.on('message', message => {
