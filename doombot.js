53e7a894bd41b4245a94fbf7975d21337e56cb7e
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-07-02 07:10:33 +0700

    Update doombot.js

--- doombot.js
+++ doombot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
  
 client.on('ready', () => {
-    client.user.setActivity({
+    client.user.setPresence({
         game: { 
             name: 'Take The Pill & Chill',
             type: 'PLAYING'
