263880c2bef8854e31b36af0f6cc2968d1a0a829
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-07-19 07:07:02 +0700

    Update doombot.js

--- doombot.js
+++ doombot.js
@@ -1,26 +1,8 @@
 const Discord = require("discord.js")
 const config = require("./config.json")
 const bot = new Discord.Client();
-const fs = require("fs");
+
 bot.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
-    return;
-  }
-
-jsfile.forEach((f, i) =>{
-  let props = require(`./commands/${f}`);
-  console.log(`${f} loaded!`);
-  bot.commands.set(props.help.name, props);
-});
-
-});
 
 
 bot.on("ready", () => {
@@ -37,22 +19,6 @@ bot.on("message", async message => {
   let args = content.slice(1);
   let prefix = config.prefix;
   if(!message.content.startsWith(prefix)) return;
-
-  //checks if message contains a command and runs it
-  let commandfile = bot.commands.get(command.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-})
-bot.on('guildMemberAdd', async member => {
-	
-	let wChan = db.fetch(`${member.guild.id}`)
-	
-	if(wChan == null) return;
-	
-	if(!wChan) return;
-		
-})
-
-
 
 
 bot.login(process.env.BOT_TOKEN);
